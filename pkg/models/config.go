e7322b4c42da5ea9c8419167f83c89193c1e9a78
fred <congruity7@gmail.com>
2022-01-18 13:55:13 +0530

    Added config new fields

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -6,7 +6,14 @@ import (
 
 type Config struct {
 	gorm.Model
-	Trash         bool
-	MaxMultiplier float32 `json:"max_multiplier"`
-	MinMultiplier float32 `json:"min_multiplier"`
+	Trash           bool
+	MaxMultiplier   float32 `json:"max_multiplier"`
+	MinMultiplier   float32 `json:"min_multiplier"`
+	Max             float32 `json:"max"`
+	Min             float32 `json:"min"`
+	SpeedSetting    float32 `json:"speed_setting"`
+	CooldownSetting float32 `json:"cooldown_setting"`
+	HouseEdge       float32 `json:"house_edge"`
+	Decimal         float32 `json:"decimal"`
+	MinTotalWager   float32 `json:"min_total_wager"`
 }
