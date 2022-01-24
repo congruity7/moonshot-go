7cb3e1f24e6467fb6c6892d286d52d04b3f735a2
fred <congruity7@gmail.com>
2022-01-24 18:57:58 +0530

    Pick env vars

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func goDotEnvVariable(key string) string {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		logrus.Error("Error loading .env file")
 	}
 
 	return os.Getenv(key)
